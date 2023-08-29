b833384f4273b5b739f5835a46f7ff9c776ec7a8
batty33 <kuraisisuxu@gmail.com>
2023-08-29 23:58:52 +0900

    test

---
+++ src/main/java/com/example/demo/TeamkoheiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamkoheiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamkoheiApplication.class, args);
+	}
+
+}
